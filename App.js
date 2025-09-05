e3f502b9cbce8c63f972950a45c0faf59df6f49a
Gangatharan M <gangatharan8504@gmail.com>
2025-09-05 11:28:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react"; 
+import TodoList from "./TodoList"; 
+import "./TodoList.css";
+const App = () => { 
+  return ( 
+    <div> 
+      <TodoList /> 
+    </div> 
+  );}; 
+export default App; 
